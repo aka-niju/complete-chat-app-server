b6fc65d2a1f5e40c1296d8c1b27b314b636ef87d
aka-niju <neeraj1510k@gmail.com>
2024-06-29 22:30:26 +0530

    cors setup 3

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,5 @@
 export const corsOptions = {
     origin: [
-        "https://complete-chat-app-server-demo.onrender.com",
         process.env.CLIENT_URL,
         "http://localhost:5173",
         "http://localhost:4173",
